7fee0fb2a1455b534230d843ffc93885e61c094c
Manoel Victor Silva Lira <manoelvictorzxc17@hotmail.com>
2024-01-10 16:39:44 -0300

    Alterações
    - Agora o formulário para senhas de usuário está adaptável a diferentes contextos de aplicação

--- EasyLiTwo/Application.Frames/PasswordConfirmation.Designer.cs
+++ EasyLiTwo/Application.Frames/PasswordConfirmation.Designer.cs
@@ -32,13 +32,15 @@
             this.label6 = new System.Windows.Forms.Label();
             this.BarraTitulo = new System.Windows.Forms.Panel();
             this.CloseButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LbPassword2 = new System.Windows.Forms.Label();
+            this.LbPassword1 = new System.Windows.Forms.Label();
             this.Password2 = new System.Windows.Forms.TextBox();
             this.Password1 = new System.Windows.Forms.TextBox();
             this.Cancel = new System.Windows.Forms.Button();
             this.Save = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.LbPassword3 = new System.Windows.Forms.Label();
+            this.Password3 = new System.Windows.Forms.TextBox();
             this.BarraTitulo.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -48,9 +50,10 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(161, 10);
+            this.label6.Location = new System.Drawing.Point(222, 12);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(112, 17);
+            this.label6.Size = new System.Drawing.Size(133, 20);
             this.label6.TabIndex = 15;
             this.label6.Text = "Confirmar senha";
             // 
@@ -61,8 +64,9 @@
             this.BarraTitulo.Controls.Add(this.CloseButton);
             this.BarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
             this.BarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.BarraTitulo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BarraTitulo.Name = "BarraTitulo";
-            this.BarraTitulo.Size = new System.Drawing.Size(432, 38);
+            this.BarraTitulo.Size = new System.Drawing.Size(577, 47);
             this.BarraTitulo.TabIndex = 17;
             // 
             // CloseButton
@@ -73,43 +77,47 @@
             this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.CloseButton.FlatAppearance.BorderSize = 0;
             this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.CloseButton.Location = new System.Drawing.Point(398, 7);
+            this.CloseButton.Location = new System.Drawing.Point(532, 9);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CloseButton.Name = "CloseButton";
-            this.CloseButton.Size = new System.Drawing.Size(22, 22);
+            this.CloseButton.Size = new System.Drawing.Size(29, 27);
             this.CloseButton.TabIndex = 4;
             this.CloseButton.UseVisualStyleBackColor = true;
             this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
-            // label1
+            // LbPassword2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(110, 100);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 17);
-            this.label1.TabIndex = 26;
-            this.label1.Text = "Repetir:";
+            this.LbPassword2.AutoSize = true;
+            this.LbPassword2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbPassword2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LbPassword2.Location = new System.Drawing.Point(147, 106);
+            this.LbPassword2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LbPassword2.Name = "LbPassword2";
+            this.LbPassword2.Size = new System.Drawing.Size(68, 20);
+            this.LbPassword2.TabIndex = 26;
+            this.LbPassword2.Text = "Repetir:";
             // 
-            // label2
+            // LbPassword1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label2.Location = new System.Drawing.Point(110, 71);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 17);
-            this.label2.TabIndex = 25;
-            this.label2.Text = "Senha:";
+            this.LbPassword1.AutoSize = true;
+            this.LbPassword1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbPassword1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LbPassword1.Location = new System.Drawing.Point(147, 70);
+            this.LbPassword1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LbPassword1.Name = "LbPassword1";
+            this.LbPassword1.Size = new System.Drawing.Size(61, 20);
+            this.LbPassword1.TabIndex = 25;
+            this.LbPassword1.Text = "Senha:";
             // 
             // Password2
             // 
             this.Password2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Password2.Location = new System.Drawing.Point(179, 97);
+            this.Password2.Location = new System.Drawing.Point(239, 101);
+            this.Password2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Password2.MaxLength = 4;
             this.Password2.Name = "Password2";
             this.Password2.PasswordChar = '•';
-            this.Password2.Size = new System.Drawing.Size(144, 23);
+            this.Password2.Size = new System.Drawing.Size(191, 26);
             this.Password2.TabIndex = 1;
             this.Password2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Password2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Password2_KeyPress);
@@ -117,11 +125,12 @@
             // Password1
             // 
             this.Password1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Password1.Location = new System.Drawing.Point(179, 68);
+            this.Password1.Location = new System.Drawing.Point(239, 67);
+            this.Password1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Password1.MaxLength = 4;
             this.Password1.Name = "Password1";
             this.Password1.PasswordChar = '•';
-            this.Password1.Size = new System.Drawing.Size(144, 23);
+            this.Password1.Size = new System.Drawing.Size(191, 26);
             this.Password1.TabIndex = 0;
             this.Password1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.Password1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Password1_KeyPress);
@@ -133,10 +142,11 @@
             this.Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Cancel.ForeColor = System.Drawing.Color.White;
-            this.Cancel.Location = new System.Drawing.Point(105, 146);
+            this.Cancel.Location = new System.Drawing.Point(140, 200);
+            this.Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(100, 35);
-            this.Cancel.TabIndex = 3;
+            this.Cancel.Size = new System.Drawing.Size(133, 43);
+            this.Cancel.TabIndex = 4;
             this.Cancel.Text = "Cancelar";
             this.Cancel.UseVisualStyleBackColor = false;
             this.Cancel.Click += new System.EventHandler(this.CancelButton_Click);
@@ -148,10 +158,11 @@
             this.Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Save.ForeColor = System.Drawing.Color.White;
-            this.Save.Location = new System.Drawing.Point(227, 146);
+            this.Save.Location = new System.Drawing.Point(303, 200);
+            this.Save.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Save.Name = "Save";
-            this.Save.Size = new System.Drawing.Size(100, 35);
-            this.Save.TabIndex = 2;
+            this.Save.Size = new System.Drawing.Size(133, 43);
+            this.Save.TabIndex = 3;
             this.Save.Text = "Confirmar";
             this.Save.UseVisualStyleBackColor = false;
             this.Save.Click += new System.EventHandler(this.Save_Click);
@@ -162,26 +173,59 @@
             this.panel1.Controls.Add(this.Password1);
             this.panel1.Controls.Add(this.BarraTitulo);
             this.panel1.Controls.Add(this.Cancel);
+            this.panel1.Controls.Add(this.Password3);
             this.panel1.Controls.Add(this.Password2);
             this.panel1.Controls.Add(this.Save);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.LbPassword1);
+            this.panel1.Controls.Add(this.LbPassword3);
+            this.panel1.Controls.Add(this.LbPassword2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(434, 194);
+            this.panel1.Size = new System.Drawing.Size(579, 260);
             this.panel1.TabIndex = 27;
+            // 
+            // LbPassword3
+            // 
+            this.LbPassword3.AutoSize = true;
+            this.LbPassword3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbPassword3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LbPassword3.Location = new System.Drawing.Point(147, 140);
+            this.LbPassword3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LbPassword3.Name = "LbPassword3";
+            this.LbPassword3.Size = new System.Drawing.Size(61, 20);
+            this.LbPassword3.TabIndex = 26;
+            this.LbPassword3.Text = "Antiga:";
+            this.LbPassword3.Visible = false;
+            // 
+            // Password3
+            // 
+            this.Password3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password3.Location = new System.Drawing.Point(239, 135);
+            this.Password3.Margin = new System.Windows.Forms.Padding(4);
+            this.Password3.MaxLength = 4;
+            this.Password3.Name = "Password3";
+            this.Password3.PasswordChar = '•';
+            this.Password3.Size = new System.Drawing.Size(191, 26);
+            this.Password3.TabIndex = 2;
+            this.Password3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Password3.Visible = false;
+            this.Password3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Password2_KeyPress);
             // 
             // PasswordConfirmation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 194);
+            this.ClientSize = new System.Drawing.Size(579, 260);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PasswordConfirmation";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Confirmação";
+            this.Load += new System.EventHandler(this.PasswordConfirmation_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PasswordConfirmation_KeyDown);
             this.BarraTitulo.ResumeLayout(false);
             this.BarraTitulo.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -195,12 +239,14 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Panel BarraTitulo;
         private System.Windows.Forms.Button CloseButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LbPassword2;
+        private System.Windows.Forms.Label LbPassword1;
         public System.Windows.Forms.TextBox Password2;
         public System.Windows.Forms.TextBox Password1;
         private System.Windows.Forms.Button Cancel;
         private System.Windows.Forms.Button Save;
         private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.TextBox Password3;
+        private System.Windows.Forms.Label LbPassword3;
     }
 }
--- EasyLiTwo/Application.Frames/PasswordConfirmation.cs
+++ EasyLiTwo/Application.Frames/PasswordConfirmation.cs
@@ -8,29 +8,26 @@ namespace EasyLiTwo.Application.Frames
     {
         private DialogResult _result;
         private string _password;
+        private string _newPassword;
+        private readonly PassworkConfirmationMode _confirmationMode;
         readonly FormatNumericInputs numericInputs = new FormatNumericInputs();
 
-        public PasswordConfirmation()
+        public PasswordConfirmation(PassworkConfirmationMode confirmationMode)
         {
             InitializeComponent();
+            _confirmationMode = confirmationMode;
         }
 
         public DialogResult GetResult => _result;
         public string GetPassword => _password;
+        public string GetNewPassword => _newPassword;
 
         private void Save_Click(object sender, EventArgs e)
         {
 
-            if (string.IsNullOrWhiteSpace(Password1.Text) || string.IsNullOrWhiteSpace(Password2.Text))
-                return;
-            else
+            if (IsPasswordValid())
             {
-                if (Password1.Text.Length < 4 || Password2.Text.Length < 4)
-                {
-                    MessageBox.Show("Sua senha deve conter quatro caracteres", "Confirmações", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    return;
-                }
-                else
+                if (_confirmationMode == PassworkConfirmationMode.NewPassword || _confirmationMode == PassworkConfirmationMode.UpdatePassword)
                 {
                     if (Password1.Text != Password2.Text)
                     {
@@ -38,13 +35,42 @@ namespace EasyLiTwo.Application.Frames
                         return;
                     }
                     else
-                    {
-                        _password = Password1.Text;
-                        _result = DialogResult.OK;
-                        Close();
-                    }
+                        UpdateAndClose();
+                }
+                else if (_confirmationMode == PassworkConfirmationMode.ConfirmPassword)
+                {
+                    //Adicionar mais rotinas se necessário e chamar a saída com UpdateAndClose();
+                    UpdateAndClose();
                 }
             }
+            else
+            {
+                MessageBox.Show("Informe os campos corretamente", "Senhas do usuário", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+        }
+
+        private void UpdateAndClose()
+        {
+            _password = Password1.Text;
+            _newPassword = Password3.Text;
+            _result = DialogResult.OK;
+            Close();
+        }
+
+        private bool IsPasswordValid()
+        {
+            switch (_confirmationMode)
+            {
+                case PassworkConfirmationMode.UpdatePassword:
+                    return !(Password1.Text.Length < 4 || Password2.Text.Length < 4 || Password3.Text.Length < 4);
+                case PassworkConfirmationMode.NewPassword:
+                    return !(Password1.Text.Length < 4 || Password2.Text.Length < 4);
+                case PassworkConfirmationMode.ConfirmPassword:
+                    return !(Password1.Text.Length < 4);
+            }
+
+            return false;
         }
 
         private void CancelButton_Click(object sender, EventArgs e)
@@ -67,6 +93,39 @@ namespace EasyLiTwo.Application.Frames
         private void Password2_KeyPress(object sender, KeyPressEventArgs e)
         {
             numericInputs.ValitedNumInput(ref e, false);
+        }
+
+        [Flags]
+        public enum PassworkConfirmationMode
+        {
+            NewPassword,
+            UpdatePassword,
+            ConfirmPassword
+        }
+
+        private void PasswordConfirmation_Load(object sender, EventArgs e)
+        {
+            KeyPreview = true;
+
+            if (_confirmationMode == PassworkConfirmationMode.UpdatePassword)
+            {
+                LbPassword3.Visible = true;
+                Password3.Visible = true;
+            }
+            else if (_confirmationMode == PassworkConfirmationMode.ConfirmPassword)
+            {
+                LbPassword2.Visible = false;
+                Password2.Visible = false;
+
+                LbPassword1.Location = LbPassword2.Location;
+                Password1.Location = Password2.Location;
+            }
+        }
+
+        private void PasswordConfirmation_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                Save.PerformClick();
         }
     }
 }
